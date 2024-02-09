ecab450b19664525842301de31cb4a1fbdb71875
DavidBlackCN <2811351820@qq.com>
2024-02-09 19:59:43 +0800

    -L2D

--- .vitepress/theme/index.mjs
+++ .vitepress/theme/index.mjs
@@ -5,33 +5,13 @@ import mediumZoom from 'medium-zoom';
 import { onMounted, watch, nextTick } from 'vue';
 import { useRoute } from 'vitepress';
 
-import { useLive2d } from 'vitepress-theme-website'
 
 export default {
   extends: DefaultTheme,
 
   setup() {
 
-        //看板娘
-        useLive2d({
-          enable: true,
-          model: {
-            url: 'https://raw.githubusercontent.com/iCharlesZ/vscode-live2d-models/master/model-library/girls-frontline/HK416-2/normal/model.json'
-          },
-          display: {
-            position: 'right',
-            width: '135px',
-            height: '300px',
-            xOffset: '35px',
-            yOffset: '5px'
-          },
-          mobile: {
-            show: true
-          },
-          react: {
-            opacity: 0.8
-          }
-        })
+
 
         
     const route = useRoute();
