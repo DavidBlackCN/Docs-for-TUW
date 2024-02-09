b2d31b4bb12e9dc0794b6d77a73a0ea3fcf4b08b
DavidBlackCN <2811351820@qq.com>
2024-02-09 18:25:32 +0800

    2024.2.9-14-noL2D

--- .vitepress/theme/index.mjs
+++ .vitepress/theme/index.mjs
@@ -5,34 +5,12 @@ import mediumZoom from 'medium-zoom';
 import { onMounted, watch, nextTick } from 'vue';
 import { useRoute } from 'vitepress';
 
-import { useLive2d } from 'vitepress-theme-website'
+//import { useLive2d } from 'vitepress-theme-website'
 
 export default {
-  ...DefaultTheme,
-  enhanceApp(ctx) {},
+  extends: DefaultTheme,
+
   setup() {
-
-        //看板娘
-        useLive2d({
-          enable: true,
-          model: {
-            url: 'https://raw.githubusercontent.com/iCharlesZ/vscode-live2d-models/master/model-library/girls-frontline/HK416-2/normal/model.json'
-          },
-          display: {
-            position: 'right',
-            width: '135px',
-            height: '300px',
-            xOffset: '35px',
-            yOffset: '5px'
-          },
-          mobile: {
-            show: true
-          },
-          react: {
-            opacity: 0.8
-          }
-        })
-
     const route = useRoute();
     const initZoom = () => {
       // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
